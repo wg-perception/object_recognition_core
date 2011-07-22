425bccd65bce01c4c0cc833e04da15fceff15a09
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-21 20:31:19 -0700

    Adding capture app.

--- src/capture/data_capture.py
+++ src/capture/data_capture.py
@@ -32,7 +32,6 @@ def parse_args():
     parser.add_argument('tags', metavar='TAG', nargs='+', type=str,
                        help='Tags to mark the object with.')
     args = parser.parse_args()
-    print args
     return args
 
 if "__main__" == __name__:
@@ -40,14 +39,14 @@ if "__main__" == __name__:
         ecto_ros.init(sys.argv, "data_capture", False)
     args = parse_args()
     plasm = ecto.Plasm()
-    sched = ecto.schedulers.Threadpool(plasm)
+    sched = ecto.schedulers.Singlethreaded(plasm)
     debug = True
-    poser = OpposingDotPoseEstimator(plasm,
-                                        rows=5, cols=3,
-                                        pattern_type=calib.ASYMMETRIC_CIRCLES_GRID,
-                                        square_size=0.04, debug=debug)
-    camera_info = calib.CameraIntrinsics('Camera Info',
-                                         camera_file="camera.yml")
+    
+    session_id = 'session_%d' % int(time.time())
+    capture_description = "data_capture.py, given a fiducial produces views that are registered to the object with R|T and produces a binary mask."
+    capture_tags = ['calibration', 'mask', 'intrinsics', 'extrinsics',
+                    'depth', 'rgb']
+
     sync = None
     if len(args.bag) == 0:
         subs = dict(image=ImageSub(topic_name='camera/rgb/image_color', queue_size=0),
@@ -64,52 +63,55 @@ if "__main__" == __name__:
                                   baggers=baggers,
                                   bag=args.bag,
                                  )
-
+    
+    poser = OpposingDotPoseEstimator(plasm,
+                                        rows=5, cols=3,
+                                        pattern_type=calib.ASYMMETRIC_CIRCLES_GRID,
+                                        square_size=0.04, debug=debug)
+    camera_info = calib.CameraIntrinsics('Camera Info',
+                                         camera_file="camera.yml")
+    
     brg2rgb = imgproc.cvtColor('bgr -> rgb', flag=4)
     rgb2gray = imgproc.cvtColor('rgb -> gray', flag=7)
     gray2rgb = imgproc.cvtColor('gray -> rgb', flag=8)
 
-    display = highgui.imshow('Poses', name='Poses', waitKey=10, autoSize=True)
+    pose_display = highgui.imshow('Poses', name='Poses', waitKey=10, autoSize=True)
     mask_display = highgui.imshow('Masks', name='Masks', waitKey= -1, autoSize=True)
-    object_display = highgui.imshow('Object', name='Object', waitKey= -1, autoSize=True)
 
-    imsaver = highgui.ImageSaver('pose image saver', filename='image_pose_')
-    rawsaver = highgui.ImageSaver('raw image saver', filename='image_raw_')
     masker = calib.PlanarSegmentation(z_min=0.015, y_crop=0.15, x_crop=0.15)
     bitwise_and = imgproc.BitwiseAnd()
     
     im2mat_rgb = ecto_ros.Image2Mat()
     im2mat_depth = ecto_ros.Image2Mat()
-    
-    session_id = 'session_%d' % int(time.time())
-    capture_description = "data_capture.py, given a fiducial produces views that are registered to the object with R|T and produces a binary mask."
-    capture_tags = ['calibration', 'mask', 'intrinsics', 'extrinsics',
-                    'depth', 'rgb']
-    capture.insert_object(args.object_id, args.description, args.tags)
-    capture.insert_session(session_id, args.object_id, capture_description, capture_tags)
+
     db_inserter = capture.ObservationInserter("db_inserter", object_id=args.object_id, session_id=session_id)
     delta_pose = capture.DeltaRT("delta R|T", angle_thresh=3.14 / 36) #5 degree increments.
     plasm.connect(
                   sync["image"] >> im2mat_rgb["image"],
                   im2mat_rgb["image"] >> (brg2rgb[:],),
                   sync["depth"] >> im2mat_depth['image'],
-                  brg2rgb[:] >> (rgb2gray[:], poser['color_image'], rawsaver['image'], bitwise_and['b']),
+                  brg2rgb[:] >> (rgb2gray[:], poser['color_image'], bitwise_and['b']),
                   rgb2gray[:] >> poser['image'],
-                  poser['debug_image'] >> (display['input'], imsaver['image']),
-                  display['out'] >> (imsaver['trigger'], rawsaver['trigger']),
+                  poser['debug_image'] >> pose_display['input'],
                   camera_info['K'] >> (poser['K'], masker['K']),
                   poser['R', 'T'] >> masker['R', 'T'],
                   im2mat_depth['image'] >> masker['depth'],
-                  masker['mask'] >> (mask_display[:], gray2rgb[:]),
+                  masker['mask'] >> gray2rgb[:],
                   gray2rgb[:] >> bitwise_and['a'],
-                  bitwise_and[:] >> object_display[:],
-                  brg2rgb[:] >> db_inserter['image'],
-                  im2mat_depth['image'] >> db_inserter['depth'],
-                  masker['mask'] >> db_inserter['mask'],
+                  bitwise_and[:] >> mask_display[:],
                   poser['R', 'T', 'found'] >> delta_pose['R', 'T', 'found'],
-                  poser['R', 'T'] >> db_inserter['R', 'T'],
-                  delta_pose['novel'] >> db_inserter['found'],
-                  camera_info['K'] >> db_inserter['K'],
                   )
+    #plasm.connect(
+#                  im2mat_depth['image'] >> db_inserter['depth'],
+#                  masker['mask'] >> db_inserter['mask'],
+#                  poser['R', 'T'] >> db_inserter['R', 'T'],
+#                  delta_pose['novel'] >> db_inserter['found'],
+#                  camera_info['K'] >> db_inserter['K'],
+#                  brg2rgb[:] >> db_inserter['image'],
+#                  )
     ecto.view_plasm(plasm)
+
+    capture.insert_object(args.object_id, args.description, args.tags)
+    capture.insert_session(session_id, args.object_id, capture_description, capture_tags)
+    
     sched.execute()
