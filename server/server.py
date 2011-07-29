ee46d63e82b2e09de1054038c4ef1e8ea1e735c2
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-29 01:10:56 -0700

    Server...

--- server/server.py
+++ server/server.py
@@ -1,40 +1,12 @@
 #!/usr/bin/env python
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-import SocketServer
-import cgi
-def bag_upload(handler):
-    global rootnode
-    print "Bag upload"
-    ctype,pdict = cgi.parse_header(handler.headers.getheader('Content-type'))
-    if ctype == 'multipart/form-data':
-        form = cgi.FieldStorage(handler.rfile,handler.headers)
-        print form.getvalue('object_name')
-    handler.send_response(301)
-    handler.end_headers()
-    handler.wfile.write('Post!')
+import CGIHTTPServer
+import BaseHTTPServer
 
-class RecognitionServer(SimpleHTTPRequestHandler):
-    get_requests = {}
-    post_requests = { '/upload/bag': bag_upload}
-    def do_GET(self):
-        if self.path in self.get_requests.keys():
-            pass
-        else:
-            SimpleHTTPRequestHandler.do_GET(self)
-
-    def do_POST(self):
-        print 'POST:',self.path
-        if self.path in self.post_requests.keys():
-            self.post_requests[self.path](self)
-
-def main():
+if __name__ == '__main__':
     try:
         PORT = 8080
-        Handler = RecognitionServer
-        httpd = SocketServer.TCPServer(("", PORT), Handler)
+        Handler = CGIHTTPServer.CGIHTTPRequestHandler
+        httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
         httpd.serve_forever()
-    except Exception,e:
+    finally:
         httpd.socket.close()
-if __name__ == '__main__':
-    main()
-
