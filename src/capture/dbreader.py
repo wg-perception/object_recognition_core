8f59b548af36cc3ae2becf49b14cadbe62259d13
Ethan Rublee <ethan.rublee@gmail.com>
2011-08-07 17:38:48 -0700

    Modifying to conform to new rosbuild_lite.

--- src/capture/dbreader.py
+++ src/capture/dbreader.py
@@ -11,7 +11,7 @@ while True:
     image_view = highgui.imshow(name="RGB", waitKey=5, autoSize=True)
     mask_view = highgui.imshow(name="mask", waitKey= -1, autoSize=True)
     depth_view = highgui.imshow(name="Depth", waitKey= -1, autoSize=True);
-    db_reader = capture.ObservationReader("db_reader", object_id="spam")
+    db_reader = capture.ObservationReader("db_reader", object_id="6ea81529b747a2e8148389481200de19")
     
     plasm.connect(db_reader, "image", image_view, "input")
     plasm.connect(db_reader, "mask", mask_view, "input")
