78fc2dc50a380f5ba0b1a66acff27e5f3edf8d19
Ethan Rublee <ethan.rublee@gmail.com>
2011-10-03 15:01:35 -0700

    Fix tests for upload and capture.

--- test/test_capture_help.py
+++ test/test_capture_help.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 import subprocess
-
-subprocess.check_call(['../apps/capture','--help'])
+import sys
+import os
+path = os.path.dirname(sys.argv[0])
+print path
+subprocess.check_call(['%s/../apps/capture'%path,'--help'])
--- test/test_upload_help.py
+++ test/test_upload_help.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 import subprocess
-
-subprocess.check_call(['../apps/upload','--help'])
+import sys
+import os
+path = os.path.dirname(sys.argv[0])
+print path
+subprocess.check_call(['%s/../apps/upload'%path,'--help'])
