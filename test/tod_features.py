5b55980fe8f923dd6a39570bbe7dc5307561af9d
Vincent Rabaud <vrabaud@willowgarage.com>
2011-10-03 10:35:54 -0700

    - more flexible interface

--- test/tod_features.py
+++ test/tod_features.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-
+"""
+module that displays a TOD model for a given object_id
+"""
 import sys
 import argparse
 import time
@@ -14,43 +16,47 @@ import ecto
 from ecto_opencv import highgui
 import object_recognition
 from object_recognition import dbtools, models, capture
+from object_recognition.common.utils.parser import ObjectRecognitionParser
 from ecto_object_recognition import tod_detection, conversion
 import ecto_pcl
-db_json_params = '''
-    {
-        "type": "CouchDB",
-        "root": "http://localhost:5984"
-    }
-'''
-db_url = dbtools.DEFAULT_SERVER_URL
 
-#database ritual
-couch = couchdb.Server(db_url)
-dbs = dbtools.init_object_databases(couch)
+if __name__ == '__main__':
+    parser = ObjectRecognitionParser()
+    dbtools.add_db_arguments(parser)
+    parser.add_argument('object_id', help='The id of the object for which the TOD model will be displayed.')
 
-object_id = "263739e4f609243242bf5ea553000bad"
+    # read the object_ids
+    args = parser.parse_args()
+    if hasattr(args, 'object_id') and args.object_id:
+        object_id = args.object_id
+    else:
+        object_id = "263739e4f609243242bf5ea553000bad"
 
-tod_models = models.find_tod_model_for_object(dbs, object_id)
-print tod_models
-if len(tod_models) < 1:
-    raise RuntimeError("There are no tod models available.")
+    #database ritual
+    couch = couchdb.Server(db_url)
+    dbs = dbtools.init_object_databases(couch)
 
-db_reader = tod_detection.ModelReader('db_reader', db_json_params=db_json_params, collection='object_recognition')
-#observation dealer will deal out each observation id.
-observation_dealer = ecto.Dealer(typer=db_reader.inputs.at('model_id'), iterable=tod_models)
-to_pcl = conversion.MatToPointCloudXYZ()
-pcl_cloud = ecto_pcl.PointCloudT2PointCloud(format=ecto_pcl.Format.XYZ)
-cloud_viewer = ecto_pcl.CloudViewer()
-plasm = ecto.Plasm()
-#View all of the observations.
-plasm.connect(
-    observation_dealer[:] >> db_reader['model_id'],
-    db_reader['points'] >> to_pcl['points'],
-    to_pcl['point_cloud'] >> pcl_cloud[:],
-    pcl_cloud[:] >> cloud_viewer[:]
-)
+    tod_models = models.find_tod_model_for_object(dbs, object_id)
 
-from ecto.opts import doit
-doit(plasm, "View observations from the database.", locals=vars())
+    if len(tod_models) < 1:
+        raise RuntimeError("There are no tod models available.")
 
-raw_input()
+    db_reader = tod_detection.ModelReader('db_reader', db_json_params=db_json_params, collection='object_recognition')
+    #observation dealer will deal out each observation id.
+    observation_dealer = ecto.Dealer(typer=db_reader.inputs.at('model_id'), iterable=tod_models)
+    to_pcl = conversion.MatToPointCloudXYZ()
+    pcl_cloud = ecto_pcl.PointCloudT2PointCloud(format=ecto_pcl.Format.XYZ)
+    cloud_viewer = ecto_pcl.CloudViewer()
+    plasm = ecto.Plasm()
+    #View all of the observations.
+    plasm.connect(
+        observation_dealer[:] >> db_reader['model_id'],
+        db_reader['points'] >> to_pcl['points'],
+        to_pcl['point_cloud'] >> pcl_cloud[:],
+        pcl_cloud[:] >> cloud_viewer[:]
+    )
+    
+    from ecto.opts import doit
+    doit(plasm, "View observations from the database.", locals=vars())
+    
+    raw_input()
