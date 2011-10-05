d09b853287254271087545a5635317f6ea912328
Ethan Rublee <ethan.rublee@gmail.com>
2011-10-04 19:20:37 -0700

    Checkpoint on mesh.

--- apps/servo_scripts/play.py
+++ apps/servo_scripts/play.py
@@ -6,6 +6,6 @@ a = ArbotiX("/dev/ttyUSB0",baud=1e6) #1 meg for e
 a.enableWheelMode(MY_SERVO)
 speed = 32
 if len(sys.argv) > 1:
-  speed = sys.argv[1]
+  speed = int(sys.argv[1])
 a.setSpeed(MY_SERVO,speed)
 
