c556aaec0cc3954f83b07d9c5ac1fcafe3f0e3f9
Vincent Rabaud <vrabaud@willowgarage.com>
2011-11-23 14:47:50 -0800

    working script

--- apps/samples/sample_training.py
+++ apps/samples/sample_training.py
@@ -23,7 +23,7 @@ db = dbtools.init_object_databases(couchdb.Server(args.db_root))
 
 for object_id in args.objects:
     #get a list of observation ids for a particular object id.
-    obs_ids = models.find_all_observations_for_object(db, object_id)[:5]
+    obs_ids = models.find_all_observations_for_object(db, object_id)
 
     if not obs_ids:
         print 'No observations found for object %s.' % object_id
@@ -43,17 +43,18 @@ for object_id in args.objects:
         path = os.path.join(object_id, path_name)
         if not os.path.exists(path):
             os.makedirs(path)
-            writer_image = ImageSaver(filename_format=os.path.join(path, '%05d.png'))[:]
-            plasm.connect(db_reader[path_name] >> (writer_image, imshow(name=path_name)['image']))
+        writer_image = ImageSaver(filename_format=os.path.join(path, '%05d.png'))[:]
+        plasm.connect(db_reader[path_name] >> (writer_image, imshow(name=path_name)['image']))
 
     sched = ecto.schedulers.Singlethreaded(plasm)
     sched.execute()
 
     # write files listing the produced files
     for path_name in path_names:
-        file_object = open(os.path.join(object_id, path_name + '.txt'), 'w')
-        for file_name in [ os.path.join(path_name, file_name) for file_name in os.listdir(os.path.join(object_id,
-                    path_name)) if file_name.endswith('png') ]:
+        file_object = open(os.path.join(os.getcwd(), object_id, path_name + '.txt'), 'w')
+        for file_name in [ os.path.join(os.getcwd(), object_id, path_name, file_name)
+                          for file_name in os.listdir(os.path.join(object_id, path_name))
+                          if file_name.endswith('png') ]:
             file_object.write(file_name + '\n')
         file_object.close
 
