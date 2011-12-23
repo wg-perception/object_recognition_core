cd04dc3b86588282c46f698fcb8ceff331c98102
Vincent Rabaud <vrabaud@willowgarage.com>
2011-12-23 00:13:09 -0800

    - simplify the configuration reading - allow different pipelines to have different soruces/sinks

--- python/object_recognition/common/utils/training_detection_args.py
+++ python/object_recognition/common/utils/training_detection_args.py
@@ -14,6 +14,39 @@ try:
 except ImportError:
     ECTO_ROS_FOUND = False
 
+def interpret_object_ids(args, source_param, pipeline_param):
+    """
+    Given command line arguments and the parameters of the pipeline, clean the 'object_ids' field to be a list of
+    object ids
+    """
+    # read some parameters
+    db_params = args_to_db_params(args, source_param)
+
+    # initialize the DB
+    db = dbtools.db_params_to_db(db_params)
+
+    # read the object_ids
+    object_ids = set()
+    for obj in (args.__dict__, pipeline_param):
+        ids = obj.get('object_ids', None)
+        names = obj.get('object_names', None)
+        if 'all' in (ids, names):
+            object_ids = set([ str(x.id) for x in models.Object.all(db) ]) #unicode without the str()
+            break
+        if 'missing' in (ids, names):
+            tmp_object_ids = set([ str(x.id) for x in models.Object.all(db) ])
+            tmp_object_ids_from_names = set([ str(x.object_id) for x in models.Model.all(db) ])
+            object_ids.update(tmp_object_ids.difference(tmp_object_ids_from_names))
+        if ids and ids != 'missing':
+            object_ids.update(ids)
+        if names and names != 'missing':
+            for object_name in names:
+                object_ids.update([str(x.id) for x in models.objects_by_name(db, object_name)])
+        # if we got some ids through the command line, just stop here
+        if object_ids:
+            break
+    return object_ids
+
 def read_arguments(parser=None, do_commit=False):
     """
     Returns:
@@ -51,45 +84,35 @@ def read_arguments(parser=None, do_commit=False):
 
     # define the input
     if args.config_file is None or not os.path.exists(args.config_file):
-        print >>sys.stderr, "The option file does not exist. --help for usage."
+        print >> sys.stderr, "The option file does not exist. --help for usage."
         sys.exit(-1)
 
     params = yaml.load(open(args.config_file))
 
-    # read some parameters
-    db_params = args_to_db_params(args, params.get('db', {}))
-
-    # initialize the DB
-    db = dbtools.db_params_to_db(db_params)
-
-    # read the object_ids
-    object_ids = set()
-    for obj in (args.__dict__, params):
-        ids = obj.get('object_ids', None)
-        names = obj.get('object_names', None)
-        if 'all' in (ids, names):
-            object_ids = set([ str(x.id) for x in models.Object.all(db) ]) #unicode without the str()
-            break
-        if 'missing' in (ids, names):
-            tmp_object_ids = set([ str(x.id) for x in models.Object.all(db) ])
-            tmp_object_ids_from_names = set([ str(x.object_id) for x in models.Model.all(db) ])
-            object_ids.update(tmp_object_ids.difference(tmp_object_ids_from_names))
-        if ids and ids != 'missing':
-            object_ids.update(ids)
-        if names and names != 'missing':
-            for object_name in names:
-                object_ids.update([str(x.id) for x in models.objects_by_name(db, object_name)])
-        # if we got some ids through the command line, just stop here
-        if object_ids:
-            break
-
-    object_ids = list(object_ids)
-    params['object_ids'] = object_ids
-
-    pipeline_params = []
+    # read the different parameters from the config file, for each pipeline
+    source_params = {}
+    pipeline_params = {}
+    sink_params = {}
     for key , value in params.iteritems():
-        if key.startswith('pipeline'):
-            pipeline_params.append(value)
+        if key.startswith('source'):
+            source_params[int(key[6:])] = value
+        elif key.startswith('pipeline'):
+            for field in [ 'object_ids', 'source', 'sink', 'method', 'submethod', 'package', 'parameters' ]:
+                if field not in value:
+                    raise RuntimeError('The pipeline parameters need to have the field "%s"' % field)
+            pipeline_params[int(key[8:])] = value
+        elif key.startswith('sink'):
+            pipeline_params[int(key[4:])] = value
+
+    # for each pipeline, make sure the corresponding source/sink exist
+    for _pipeline_id, pipeline_param in pipeline_params.iteritems():
+        if pipeline_param['source'] not in source_params:
+            raise RuntimeError('The pipeline parameters has an invalid source number')
+        if pipeline_param['sink'] not in sink_params:
+            raise RuntimeError('The pipeline parameters has an invalid sink number')
+        # clean the object_ids
+        pipeline_param['object_ids'] = interpret_object_ids(args, source_params[pipeline_param['source']],
+                                                            pipeline_param)
 
     args = vars(args)
-    return params, args, pipeline_params, args['visualize'], db_params
+    return source_params, pipeline_params, sink_params, args
