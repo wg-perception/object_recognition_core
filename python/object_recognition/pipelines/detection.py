9ad3cbb92006e755ca4e61fab405417c0eb78d11
Vincent Rabaud <vrabaud@willowgarage.com>
2012-01-10 15:06:05 -0800

    obey the new API

--- python/object_recognition/pipelines/detection.py
+++ python/object_recognition/pipelines/detection.py
@@ -44,7 +44,7 @@ class DetectionPipeline:
             raise NotImplementedError("The detector needs to have a 'pose_results' output tendril.")
         # check for the right type for the pose_results output
         type_name = detector.outputs.at('pose_results').type_name
-        if type_name not in ['std::vector<object_recognition::io::PoseResult, std::allocator<object_recognition::io::PoseResult> >']:
+        if type_name not in ['std::vector<object_recognition::common::PoseResult, std::allocator<object_recognition::common::PoseResult> >']:
             raise NotImplementedError("The detector does not have a 'pose_results' tendril of the right type.\n"
                                       "Must have an output named 'pose_results', with type %s\n"
                                       "This cells output at 'pose_results' has type %s" % ('std::vector<PoseResult>', type_name))
