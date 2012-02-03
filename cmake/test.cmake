28858b2cd1c84a534a9247121d464c32c2aa83a6
Vincent Rabaud <vrabaud@willowgarage.com>
2012-02-02 22:19:56 -0800

    make sure it works with catkin on Fuerte

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -2,7 +2,7 @@
 macro(object_recognition_pytest pyfile)
   string(REPLACE ";" " " args "${ARGN}")
   add_test(${PROJECT_NAME}_${pyfile}
-    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/python_path.sh ${CMAKE_CURRENT_SOURCE_DIR}/${pyfile}.py ${args}
+    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/setup.sh ${CMAKE_CURRENT_SOURCE_DIR}/${pyfile}.py ${args}
     )
 endmacro()
 
@@ -10,7 +10,7 @@ macro(object_recognition_desktop_test pyfile)
   if(DESKTOP_TEST)
     string(REPLACE ";" " " args "${ARGN}")
     add_test(desktop_${PROJECT_NAME}_${pyfile}
-      ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/python_path.sh ${CMAKE_CURRENT_SOURCE_DIR}/${pyfile}.py ${args}
+      ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/setup.sh ${CMAKE_CURRENT_SOURCE_DIR}/${pyfile}.py ${args}
       )
   endif(DESKTOP_TEST)
 endmacro()
@@ -27,20 +27,20 @@ endmacro()
 # macro testing a detection pipeline
 macro(object_recognition_detection_test config_file)
   add_test(${PROJECT_NAME}_detection
-    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/python_path.sh "${object_recognition_SHARE_DIRS}/test/test_detection_pipeline.py -c ${config_file} --node_name \"\""
+    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/setup.sh "${object_recognition_SHARE_DIRS}/test/test_detection_pipeline.py -c ${config_file} --node_name \"\""
     )
 endmacro()
 
 # macro testing a training pipeline
 macro(object_recognition_training_test config_file)
   add_test(${PROJECT_NAME}_training
-    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/python_path.sh "${object_recognition_SHARE_DIRS}/test/test_training_pipeline.py -c ${config_file} --node_name \"\""
+    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/setup.sh "${object_recognition_SHARE_DIRS}/test/test_training_pipeline.py -c ${config_file} --node_name \"\""
     )
 endmacro()
 
 # macro testing a script with --help: "script" must contain the whole path
 macro(object_recognition_help_test script)
   add_test(${PROJECT_NAME}_help_script
-    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/python_path.sh "${object_recognition_SHARE_DIRS}/test/test_help.py ${script}"
+    ${object_recognition_SHARE_DIRS}/test/run_test.sh ${CMAKE_BINARY_DIR}/setup.sh "${object_recognition_SHARE_DIRS}/test/test_help.py ${script}"
     )
 endmacro()
