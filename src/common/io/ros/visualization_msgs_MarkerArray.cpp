bf2dcbbbb85266f82f1a4ec7565c80d899ea1900
Vincent Rabaud <vrabaud@willowgarage.com>
2012-02-15 17:57:58 -0800

    add a marker publisher

--- src/common/io/ros/visualization_msgs_MarkerArray.cpp
+++ src/common/io/ros/visualization_msgs_MarkerArray.cpp
@@ -9,7 +9,19 @@
 #include <ecto_ros/wrap_bag.hpp>
 
 
+#include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+
+namespace io_ros
+{
+    struct Subscriber_Marker : ecto_ros::Subscriber<visualization_msgs::Marker> {};
+    struct Publisher_Marker : ecto_ros::Publisher<visualization_msgs::Marker> {};
+    struct Bagger_Marker : ecto_ros::Bagger<visualization_msgs::Marker> {};
+}
+
+ECTO_CELL(io_ros, io_ros::Subscriber_Marker,"Subscriber_Marker", "Subscribes to a visualization_msgs::Marker.");
+ECTO_CELL(io_ros, io_ros::Publisher_Marker,"Publisher_Marker", "Publishes a visualization_msgs::Marker.");
+ECTO_CELL(io_ros, io_ros::Bagger_Marker,"Bagger_Marker", "A bagger for messages of a given type. Can enable read/write to ros bags.");
 
 namespace io_ros
 {
