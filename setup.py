5ba395633af6d6768d86ad09fdec149c9be3a014
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 19:41:37 -0700

    use catkin for Python

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Object recognition core',
+      version='1.0.0',
+      description='The core of object recognition',
+      packages=['object_recognition_core'],
+      package_dir={'':'python'}
+)
