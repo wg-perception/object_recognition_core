3af4f9f5b48f20927b6d6fe8e909426c6ecabb02
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-22 16:23:56 -0500

    fix more import

--- python/object_recognition_core/db/models.py
+++ python/object_recognition_core/db/models.py
@@ -1,7 +1,7 @@
 from couchdb.design import ViewDefinition
 from couchdb.mapping import TextField, ListField, DateTimeField, Document, ViewField, IntegerField
 from datetime import datetime
-import tools as dbtools
+import object_recognition_core.db.tools as dbtools
 from object_recognition_core.db.tools import DEFAULT_DB_ROOT, init_object_databases
 import couchdb
 
