7c9be436626d5a5efe77020fca9165a7bbebd28d
Vincent Rabaud <vrabaud@willowgarage.com>
2012-06-05 11:09:12 -0500

    install to shar for rosrun to work

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -5,8 +5,18 @@ install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/
 )
 
 # install the share folder
-install(DIRECTORY ${CMAKE_BINARY_DIR}/share/
+install(DIRECTORY ${CMAKE_BINARY_DIR}/share/test
         DESTINATION share/${PROJECT_NAME}
         COMPONENT main
         USE_SOURCE_PERMISSIONS
+)
+
+# install the applications
+install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
+                 ${PROJECT_SOURCE_DIR}/apps/training
+        DESTINATION ${object_recognition_core_DIR}/bin
+)
+
+install(FILES ${PROJECT_SOURCE_DIR}/apps/roscompat.py
+        DESTINATION ${object_recognition_core_DIR}/bin
 )
