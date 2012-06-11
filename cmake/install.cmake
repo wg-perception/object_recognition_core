00f3b6108113c77e1b641b07aebae0a0353ac2c5
Vincent Rabaud <vrabaud@willowgarage.com>
2012-06-11 12:10:04 -0700

    install the manifest.xml

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -27,4 +27,7 @@ install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
                  ${PROJECT_SOURCE_DIR}/apps/training
         DESTINATION share/${PROJECT_NAME}/bin
 )
+install(FILES ${PROJECT_SOURCE_DIR}/manifest.xml
+        DESTINATION share/${PROJECT_NAME}
+)
 endif()
