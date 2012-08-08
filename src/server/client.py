5eeff30638ed742b09ce74e311c2ed532c149f69
Vincent Rabaud <vrabaud@willowgarage.com>
2012-08-08 14:35:06 -0700

    use the new messages

--- src/server/client.py
+++ src/server/client.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     print 'rospy args stripped',args
     if args != ['--help']:
         rospy.init_node('recognition_client')
-        client = actionlib.SimpleActionClient('recognize_objects', RecognizeObjectsAction)
+        client = actionlib.SimpleActionClient('recognize_objects', ObjectRecognitionAction)
         client.wait_for_server()
     
         start = rospy.Time.now() # for checking the round trip time.
-        client.send_goal(RecognizeObjectsGoal(),done_cb=on_result)
+        client.send_goal(ObjectRecognitionGoal(),done_cb=on_result)
         client.wait_for_result() # wait indefinitely for a result
     
         #print out the round trip time.
