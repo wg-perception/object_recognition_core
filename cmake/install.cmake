b9a78234ad4f2b8c6833f129837423a952faef9f
Vincent Rabaud <vrabaud@willowgarage.com>
2012-09-10 12:07:29 +0200

    improve installation on Groovy

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,3 +1,4 @@
+if (ROS_ELECTRIC_FOUND OR ROS_FUERTE_FOUND)
 # install the include folder
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/
         DESTINATION include
@@ -10,6 +11,7 @@ install(DIRECTORY ${CMAKE_BINARY_DIR}/share/test
         COMPONENT main
         USE_SOURCE_PERMISSIONS
 )
+endif()
 
 if (ROS_ELECTRIC_FOUND)
 # install the applications
@@ -21,7 +23,9 @@ install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
 install(FILES ${PROJECT_SOURCE_DIR}/apps/roscompat.py
         DESTINATION bin
 )
-else()
+endif()
+
+if (ROS_FUERTE_FOUND)
 # install the applications
 install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
                  ${PROJECT_SOURCE_DIR}/apps/training
@@ -29,9 +33,34 @@ install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
 )
 
 install(FILES ${PROJECT_SOURCE_DIR}/apps/roscompat.py
-        DESTINATION bin
+        DESTINATION share/${PROJECT_NAME}/bin
 )
 install(FILES ${PROJECT_SOURCE_DIR}/manifest.xml
         DESTINATION share/${PROJECT_NAME}
+)
+endif()
+
+if (ROS_GROOVY_OR_ABOVE_FOUND)
+# install the include folder
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/object_recognition_core/
+        DESTINATION ${CATKIN_PROJECT_INCLUDE_DESTINATION}
+        COMPONENT main
+)
+ 
+# install the share folder
+install(DIRECTORY ${CATKIN_BUILD_PREFIX}/${CATKIN_PROJECT_SHARE_DESTINATION}/test
+        DESTINATION ${CATKIN_PROJECT_SHARE_DESTINATION}
+        COMPONENT main
+        USE_SOURCE_PERMISSIONS
+)
+
+# install the applications
+install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
+                 ${PROJECT_SOURCE_DIR}/apps/training
+        DESTINATION ${CATKIN_PROJECT_BIN_DESTINATION}
+)
+
+install(FILES ${PROJECT_SOURCE_DIR}/apps/roscompat.py
+        DESTINATION ${CATKIN_PROJECT_BIN_DESTINATION}
 )
 endif()
