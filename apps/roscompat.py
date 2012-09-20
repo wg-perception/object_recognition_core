fb4dc788a603aef329aba983d69324ee89e44283
Vincent Rabaud <vrabaud@willowgarage.com>
2012-09-20 14:28:59 +0200

    remove roscompat

--- apps/roscompat.py
+++
@@ -1,9 +0,0 @@
-try:
-    import rospkg
-    import os
-    if rospkg.on_ros_path(os.path.dirname(__file__)):
-        import roslib; roslib.load_manifest('object_recognition_core')
-except ImportError:
-    pass
-except Exception as e :
-    pass
