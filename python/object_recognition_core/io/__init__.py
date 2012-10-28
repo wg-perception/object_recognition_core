278d180c130c494167dc1060d2588ec0bcfa51e2
Vincent Rabaud <vrabaud@willowgarage.com>
2012-10-28 00:05:39 -0700

    fix a few compatibility issues

--- python/object_recognition_core/io/__init__.py
+++ python/object_recognition_core/io/__init__.py
@@ -1,17 +1,3 @@
-"""
-Next lines inspired from object_recognition_core/db/__init__.py
-"""
-
-from object_recognition_core.io.sink import *
-from object_recognition_core.io.source import *
-
-try:
-    import ecto_ros
-    ECTO_ROS_FOUND = True
-except ImportError:
-    ECTO_ROS_FOUND = False
-
-if ECTO_ROS_FOUND:
-    from object_recognition_core.io.ros import *
-
-from object_recognition_core.io.voter import *
+from .sink import *
+from .source import *
+from .voter import *
