a021ce4169567b1dbe6f6193c63989fde4c1b0c3
Vincent Rabaud <vrabaud@willowgarage.com>
2013-01-21 11:53:00 +0100

    CMake cleanups and fixes #4

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,10 +1,4 @@
 if (ROS_FUERTE_FOUND)
-# install the include folder
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/
-        DESTINATION include
-        COMPONENT main
-)
-
 # install the share folder
 install(DIRECTORY ${CMAKE_BINARY_DIR}/share/test
         DESTINATION share/${PROJECT_NAME}
@@ -14,34 +8,28 @@ install(DIRECTORY ${CMAKE_BINARY_DIR}/share/test
 endif()
 
 if (ROS_FUERTE_FOUND)
-# install the applications
-install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
-                 ${PROJECT_SOURCE_DIR}/apps/training
-        DESTINATION share/${PROJECT_NAME}/bin
-)
-
 install(FILES ${PROJECT_SOURCE_DIR}/manifest.xml
         DESTINATION share/${PROJECT_NAME}
 )
 endif()
 
-if (ROS_GROOVY_OR_ABOVE_FOUND)
 # install the include folder
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/object_recognition_core/
         DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
         COMPONENT main
 )
- 
+
+if (ROS_GROOVY_OR_ABOVE_FOUND)
 # install the share folder
 install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION}/test
         DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
         COMPONENT main
         USE_SOURCE_PERMISSIONS
 )
+endif()
 
 # install the applications
 install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
                  ${PROJECT_SOURCE_DIR}/apps/training
         DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
 )
-endif()
