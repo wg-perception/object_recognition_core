e05a73d2ec9e8942b38495ae73d57b3d8e148b68
Vincent Rabaud <vrabaud@willowgarage.com>
2013-05-08 16:14:55 +0000

    split K in K_image and K_depth as done upstream

--- python/object_recognition_core/io/source.py
+++ python/object_recognition_core/io/source.py
@@ -44,11 +44,11 @@ def validate_source(cell):
     assert(isinstance(cell, SourceBase))
     outputs = dir(cell.outputs)
     #all sources must produce the following
-    for x in ('K', 'image', 'depth'):
+    for x in ('K_depth', 'K_image', 'image', 'depth'):
         if x not in outputs:
             raise NotImplementedError('This cell with doc\n%s\ndoes not correctly implement the source interface. Must have an output named %s' % (cell.__doc__, x))
     #type checks
-    for x in ('K', 'image', 'depth'):
+    for x in ('K_depth', 'K_image', 'image', 'depth'):
         type_name = cell.outputs.at(x).type_name
         #TODO add more explicit types.
         if type_name != 'cv::Mat':
