a49777aadd50708bab17b25c428ba485784d4c77
Vincent Rabaud <vrabaud@willowgarage.com>
2014-01-25 21:18:10 +0100

    install the db_scripts

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup()
-
-d['packages'] = ['object_recognition_core', 'object_recognition_core.db', 'object_recognition_core.filters',
+d = generate_distutils_setup(
+    packages=['object_recognition_core', 'object_recognition_core.db', 'object_recognition_core.filters',
                 'object_recognition_core.io', 'object_recognition_core.pipelines',
-                'object_recognition_core.utils', 'couchdb']
-d['package_dir'] = {'': 'python'}
-d['install_requires'] = []
+                'object_recognition_core.utils', 'couchdb'],
+    package_dir={'': 'python'},
+    scripts=['apps/detection', 'apps/training', 'apps/dbscripts/copy_db.py',
+             'apps/dbscripts/mesh_add.py', 'apps/dbscripts/object_add.py',
+             'apps/dbscripts/object_delete.py', 'apps/dbscripts/object_search.py']
+)
 
 setup(**d)
