3492b6963153fbcd097d50613120070717399d2d
edgarriba <edgar.riba@gmail.com>
2014-08-30 21:13:56 +0200

    OpenCV 3.0 adaptation

--- src/db/opencv.cpp
+++ src/db/opencv.cpp
@@ -89,7 +89,7 @@ namespace object_recognition_core
       std::streampos length = ss.tellp();
       std::vector<uint8_t> buffer(length);
       ss.read((char*) buffer.data(), length);
-      image = cv::imdecode(buffer, CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_ANYCOLOR);
+      image = cv::imdecode(buffer, cv::IMREAD_ANYDEPTH | cv::IMREAD_ANYCOLOR);
     }
   }
 
