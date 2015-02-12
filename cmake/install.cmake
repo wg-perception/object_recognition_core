52154c5fbb16d3aa8a7ac5a5a15615dd3b989584
Vincent Rabaud <vincent.rabaud@gmail.com>
2015-02-12 16:31:55 +0100

    Properly install test macros This fixes #33

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -4,13 +4,6 @@ install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/object_recognition_core/
         COMPONENT main
 )
 
-# install the share folder
-install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION}/test
-        DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
-        COMPONENT main
-        USE_SOURCE_PERMISSIONS
-)
-
 # install the applications
 install(PROGRAMS ${PROJECT_SOURCE_DIR}/apps/detection
                  ${PROJECT_SOURCE_DIR}/apps/training
